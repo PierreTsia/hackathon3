4a40ccea499a24e61f4017bed0c5e1f65da81b15
Pierre_T <pierre.tsiakkaros@gmail.com>
2017-12-13 23:08:56 +0100

    test http request

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
+
+
   <div id="app">
-    <img src="./assets/logo.png">
+    <test></test>
+    <h2>this is app</h2>
+   
+    
     <router-view/>
   </div>
+  
+
+
 </template>
 
+
+
 <script>
+import Test from './components/test.vue'
+
 export default {
-  name: 'app'
+  name: 'app', 
+  components: {
+            'test': Test
+        }
 }
+
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
