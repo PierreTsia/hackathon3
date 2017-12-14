8902aded53f0ace9d54baf179cc33ee28852fd9a
Pierre_T <pierre.tsiakkaros@gmail.com>
2017-12-14 17:58:42 +0100

    post avec props dynamiques

--- src/components/model.vue
+++ src/components/model.vue
@@ -1,78 +1,100 @@
 <template>
   <div id="app-model">
-      <h2>Modèles de placements proposés:</h2>
-       <ul>
-      <li v-for="(modelAsset, index) in modelAssets">{{modelAsset.name}} <br>
+    <h2>Modèles de placements proposés:</h2>
+    <ul>
+      <li v-for="(modelAsset, index) in modelAssets">Nom :{{modelAsset.name}} <br>
+     
         <input v-model="modelAssetAmount">
-        <button v-on:click="addAmount(index)">X</button>
-   
-
+        <button v-on:click="getAssetInfo(modelAsset, index)">X</button>
+  
+  
       </li>
     </ul>
-
+  
     <button v-on:click="postNewAsset()">Simulation</button>
   </div>
 </template>
 
 
 <script>
-import axios from "axios";
-import {mapGetters} from "vuex"
-export default {
-  name: "model",
-    computed : {
-    ...mapGetters(['modelAssets'])
-  },
-  data() {
-    return {
-      ajout: {
-        /* vont ensemble, même form, donc ajout. aux 2 */
-
-        modelAssetAmount: ""
-      },
-      results: [],
-      assets: [],
-      errors: [],
-      newAsset: {}
-      // errors:
-    };
-  },
-  methods: {
-    // Fetches posts when the component is created.
-   
-
-    addAmount: function(index) {
-      console.log(this.modelAssetAmount);
-      console.log(index);
+  import axios from "axios";
+  import {
+    mapGetters
+  } from "vuex"
+  
+  
+  export default {
+    name: "model",
+    computed: {
+      ...mapGetters(['modelAssets'])
     },
+    data() {
+      return {
+        ajout: {
+          /* vont ensemble, même form, donc ajout. aux 2 */
+  
+          modelAssetAmount: "",
+          modelAssetName: "",
+          modelAssetBaseTax: "",
+          modelAssetFeesSetup: "",
+          modelAssetFeesYear: "",
+          modelAssetIdAssetCategory: "",
+          modelAssetIdAssetModel: "",
+          modelAssetIdAssetType: "",
+          modelAssetIdDetention: "",
+          modelAssetRate: "",
+          modelAssetRatioLiquidity: "",
+          modelAssetRatioRisk: "",
+        },
+  
+        results: [],
+        assets: [],
+        errors: [],
+        newAsset: {}
+        // errors:
+      };
+    },
+    methods: {
+      // Fetches posts when the component is created.
+  
+  
+      getAssetInfo: function(modelAsset, index) {
+        this.modelAssetName = modelAsset.name;
+        this.modelAssetRate = modelAsset.rate;
+        this.modelAssetIdAssetModel = modelAsset.idAssetModel;
 
-    async postNewAsset() {
-      this.newAsset.idUser = 4;
-      this.newAsset.idAssetModel = 1;
-      this.newAsset.name = "Assurance vie";
-      this.newAsset.amount = this.modelAssetAmount;
-      this.newAsset.rate = 1.5;
-      this.newAsset.start = "2017-12-14T00:00:00.000Z";
-      this.newAsset.end = null;
-      try {
-        const response = await axios.post(
-          "https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/asset",
-          this.newAsset
-        );
-        console.log(this.newAsset);
-      } catch (e) {
-        this.error.push(e);
+  
+        console.log(modelAsset);
+        console.log(index);
+      },
+  
+      async postNewAsset() {
+        this.newAsset.idUser = 4;
+        this.newAsset.idAssetModel = this.modelAssetIdAssetModel;
+        this.newAsset.name = this.modelAssetName;
+        this.newAsset.amount = this.modelAssetAmount;
+        this.newAsset.rate = this.modelAssetRate;
+        this.newAsset.start = "2017-12-14T00:00:00.000Z";
+        this.newAsset.end = null;
+        try {
+          const response = await axios.post(
+            "https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/asset",
+            this.newAsset
+          );
+          console.log(this.newAsset);
+        } catch (e) {
+          this.error.push(e);
+        }
+        this.$store.dispatch("GET_CURRENT_ASSETS");
       }
-      this.getCurrentAsset();
+    },
+    mounted() {
+      this.$store.dispatch('GET_MODEL_ASSETS')
+  
     }
-  },
-  mounted() {
-     this.$store.dispatch('GET_MODEL_ASSETS')
-
-  }
-};
+  };
 </script>
 
 <style>
-
+  
 </style>
--- src/components/patrimoine.vue
+++ src/components/patrimoine.vue
@@ -4,7 +4,8 @@
     <h2>Votre patrimoine actif:</h2>
 
     <p v-for="(asset, index) in assets"> Nom : {{asset.name}} <br>
-      Solde : {{asset.amount}} <br>Taux : {{asset.rate}} %
+      Solde : {{asset.amount}} <br>Taux : {{asset.rate}} % <br>
+       Taux de Risque : {{asset.ratioRisk}}
     </p>
     <button>DELETE QUI FAIT RIEN ENCORE</button>
   </div>
@@ -29,18 +30,7 @@ export default {
     };
   },
   methods: {
-    // Fetches posts when the component is created.
-    /*   async getCurrentAsset() {
-      try {
-        const response = await axios.get(
-     
-          `https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/4/asset/`
-        );
-        this.assets = response.data;
-      } catch (e) {
-        this.error.push(e);
-      }
-    } */
+    
   },
   created() {
     this.$store.dispatch("GET_CURRENT_ASSETS");
