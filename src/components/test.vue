f021f76e138daccf99c3fcd2f324b8e138c6e4cf
Pierre_T <pierre.tsiakkaros@gmail.com>
2017-12-14 13:42:19 +0100

    addmodel + json.parse

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,37 +1,52 @@
 <template>
-<div>
-    <p v-for="asset of assets"> Nom : {{asset.name}} <br>
-    Solde : {{asset.amount}} <br>Taux : {{asset.rate}} %
-
-    </p>
-
-    
-</div>
-
+  <div>
+    <ul>
+      <li v-for="asset of assets"> Nom : {{asset.name}} <button v-on:click="addModel(asset)">Add</button><br> Solde : {{asset.amount}} <br>Taux : {{asset.rate}} %
+      <br>Ratio risque :{{asset.ratioRisk}} <br>Ratio Liquidity : {{asset.ratioLiquidity}}
+      
   
+      </li>
+  
+    </ul>
+  
+  
+  </div>
 </template>
 
 
 
 <script>
-import axios from "axios";
-export default {
-  name: "test",
-  data() {
-    return {
-      assets: [],
-      errors: []
-    };
-  },
+  import axios from "axios";
+  export default {
+    name: "test",
+    data() {
+      return {
+        assets: [],
+        errors: [],
+        cart:[]
+      };
+    },
+    methods : {
+      addModel: function (input){
+        this.cart.push(input);
+        console.log(this.cart)
+      }
 
-  // Fetches posts when the component is created.
- async created() {
-    try{
-        const response = await axios.get(`https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/4/asset/`)
-        this.assets = response.data;
-   } catch(e){
-       this.error.push(e)
-   } 
-  }
-}
+    },
+  
+    // Fetches posts when the component is created.
+    async created() {
+      try {
+        const response = await axios.get(
+          `https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/asset/model`
+        );
+        this.assets = JSON.parse(response.data.body);
+        console.log(this.assets)
+      } catch (e) {
+        this.error.push(e);
+      }
+    }
+
+    
+  };
 </script>
